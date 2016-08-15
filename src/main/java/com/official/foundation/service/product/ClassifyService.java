7d3f45371b85e2c83e4eeb2b48bb011799e384eb
ShawnXII <wang492715@foxmail.com>
2016-08-15 19:28:03 +0800

    tijiao

--- src/main/java/com/official/foundation/service/product/ClassifyService.java
+++ src/main/java/com/official/foundation/service/product/ClassifyService.java
@@ -1,39 +1,36 @@
 package com.official.foundation.service.product;
 
-import java.util.Map;
+import java.util.List;
 
 import org.apache.commons.lang3.StringUtils;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.official.core.base.search.support.SearchOperator;
 import com.official.core.base.search.support.Searchable;
-import com.official.core.util.BeanUtils;
 import com.official.foundation.domain.po.product.Classify;
 import com.official.foundation.facade.product.ClassifyFacadeService;
 import com.official.foundation.service.BaseService;
 
 @Service
-public class ClassifyService extends BaseService<Classify,Long> implements ClassifyFacadeService{
+public class ClassifyService extends BaseService<Classify, Long>implements ClassifyFacadeService {
 
 	@Override
-	public  Map<String, Object> searchClassify(Classify classify, Integer pageIndex, Integer pageSize) {
-		Searchable searchable=Searchable.newSearchable();
-		searchable.addSearchFilter("deleteStatus", SearchOperator.eq,Boolean.valueOf(false));
-		if(classify!=null){
-			String title=classify.getTitle();
-			if(StringUtils.isNotBlank(title)){
+	public List<Classify> searchClassify(Classify classify) {
+		Searchable searchable = Searchable.newSearchable();
+		searchable.addSearchFilter("deleteStatus", SearchOperator.eq, Boolean.valueOf(false));
+		if (classify != null) {
+			String title = classify.getTitle();
+			if (StringUtils.isNotBlank(title)) {
 				searchable.addSearchFilter("title", SearchOperator.like, title);
 			}
+			String createBy=classify.getCreateBy();
+			if(StringUtils.isNotBlank(createBy)){
+				searchable.addSearchFilter("createBy", SearchOperator.eq, createBy);
+			}
 		}
-		pageIndex=pageIndex<0?0:pageIndex;
-		pageSize=(pageSize<1||pageSize>200)?20:pageSize;
-		searchable.setPage(pageIndex, pageSize);
 		searchable.addSort(Direction.DESC, "weight");
-		Page<Classify> page=super.findAll(searchable);
-		return BeanUtils.PageToMap(page);
+		return super.findAllWithSort(searchable);
 	}
-	
-	
+
 }
--- src/main/java/com/official/foundation/service/product/SpecificationService.java
+++ src/main/java/com/official/foundation/service/product/SpecificationService.java
@@ -1,7 +1,11 @@
 package com.official.foundation.service.product;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.official.core.base.search.support.SearchOperator;
+import com.official.core.base.search.support.Searchable;
 import com.official.foundation.domain.po.product.Specification;
 import com.official.foundation.facade.product.SpecificationFacadeService;
 import com.official.foundation.service.BaseService;
@@ -9,4 +13,26 @@ import com.official.foundation.service.BaseService;
 @Service
 public class SpecificationService extends BaseService<Specification, Long>implements SpecificationFacadeService {
 
+	@Override
+	public boolean checkTitle(String title) {
+		Searchable searchable=Searchable.newSearchable();
+		searchable.addSearchFilter("title", SearchOperator.eq, title);
+		List<Specification> list=this.findAllWithNoPageNoSort(searchable);
+		if(list!=null&&list.size()>0){
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public List<Specification> searchSpec(Specification specification) {
+		Searchable searchable=Searchable.newSearchable();
+		if(specification!=null){
+			if(specification!=null&&specification.getStoreId()>0){
+				searchable.addSearchFilter("storeId", SearchOperator.eq, specification.getStoreId());
+			}
+		}
+		return this.findAllWithNoPageNoSort(searchable);
+	}
+	
 }
