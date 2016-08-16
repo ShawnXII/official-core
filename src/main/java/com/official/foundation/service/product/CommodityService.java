1d9a52e3f27452a31f60ba8b213a6c232b6544e9
ShawnXII <wang492715@foxmail.com>
2016-08-16 19:09:07 +0800

    tijiao

--- src/main/java/com/official/foundation/service/product/CommodityService.java
+++ src/main/java/com/official/foundation/service/product/CommodityService.java
@@ -1,12 +1,35 @@
 package com.official.foundation.service.product;
 
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
+import com.official.core.base.search.support.SearchOperator;
+import com.official.core.base.search.support.Searchable;
+import com.official.core.util.BeanUtils;
 import com.official.foundation.domain.po.product.Commodity;
 import com.official.foundation.facade.product.CommodityFacadeService;
 import com.official.foundation.service.BaseService;
 
 @Service
-public class CommodityService extends BaseService<Commodity,Long> implements CommodityFacadeService{
-	
+public class CommodityService extends BaseService<Commodity, Long>implements CommodityFacadeService {
+
+	@Override
+	public Map<String, Object> searchCommodity(Commodity commodity, Integer pageIndex, Integer pageSize) {
+		Searchable searchable = Searchable.newSearchable();
+		searchable.addSearchFilter("deleteStatus", SearchOperator.eq, Boolean.FALSE);
+		if (commodity != null) {
+			if (StringUtils.isNotBlank(commodity.getTitle())) {
+				searchable.addSearchFilter("title", SearchOperator.like, commodity.getTitle());
+			}
+		}
+		pageIndex = pageIndex < 0 ? 0 : pageIndex;
+		pageSize = (pageSize < 0 || pageSize > 200) ? 200 : pageSize;
+		searchable.setPage(pageIndex, pageSize);
+		Page<Commodity> page = this.findAll(searchable);
+		return BeanUtils.PageToMap(page);
+	}
+
 }
